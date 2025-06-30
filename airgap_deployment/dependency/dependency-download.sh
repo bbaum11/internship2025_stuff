e55b5ea5d846e0677ee5537c224acab3d9c60e55
bbaum11 <149198336+bbaum11@users.noreply.github.com>
2025-06-30 14:20:28 -0400

    Update dependency-download.sh

--- airgap_deployment/dependency/dependency-download.sh
+++ airgap_deployment/dependency/dependency-download.sh
@@ -11,7 +11,6 @@
 # to extract the tarball:
 # tar -xzvf dependencies.tar.gz
 
-
 set -e
 
 mkdir -p files && cd files
@@ -39,6 +38,54 @@ if [ "${#images[@]}" -gt 0 ]; then
     podman save "${images[@]}" | gzip > all-images.tar.gz
 fi
 
-tar -czvf dependencies.tar.gz files all-images.tar.gz
+# adding the retag and push script into a local file and adding it to the archive file
+cat > retag-and-push.sh <<'EOF'
+#!/bin/bash
 
-rm -r files all-images.tar.gz
+# this script takes all locally cached container images that start with docker.io, 
+# quay.io, ghcr.io, and localhost and retags/pushes them to a specified docker registry.
+# the hostname used for the registry should be the IP instead of localhost (if the server
+# is on the system running this script) so that the k8s instance can pull from it if it
+# is on another system.
+
+if [ "$#" -eq 0 ]; then
+  echo "please provide an address for the registry"
+  exit 1
+elif [[ $1 == *localhost* ]]; then
+    echo "please make sure to use the IP address of the registry if it is on this system."
+    exit 1
+elif [[ $(curl -s "${1}/v2/_catalog" -o /dev/null -w "%{http_code}") != "200" ]]; then
+    echo "please provide a valid docker registry"
+    exit 1
+fi
+
+registry_address=$1
+
+registries=("docker.io" "quay.io" "ghcr.io" "localhost")
+
+for prefix in "${registries[@]}"; do
+    while IFS= read -r image; do
+        trimmed_name="${image#${prefix}/}"
+        new_path="${registry_address}/${trimmed_name}"
+
+        echo "tagging: $image -> $new_path"
+        podman tag "$image" "$new_path"
+
+        echo "pushing: $new_path"
+        podman push "${new_path}"
+        if [ $? -ne 0 ]; then
+            echo "failed pushing $new_path"
+        else
+            echo "pushed $new_path"
+        fi
+    done < <(podman images --format '{{.Repository}}:{{.Tag}}' | grep "^${prefix}/")
+done
+
+echo "done!"
+EOF
+
+
+
+tar -czvf dependencies.tar.gz files all-images.tar.gz retag-and-push.sh
+
+rm -r files all-images.tar.gz retag-and-push.sh
